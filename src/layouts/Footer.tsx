ae0b67e14298c4cf4fc048df63affdf8d68e103e
LeulBerhanu <leulberhanua@gmail.com>
2024-11-18 17:39:23 +0300

    create Footer component

---
+++ src/layouts/Footer.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import NavLinks from "../components/NavLinks";
+import Logo from "../components/Logo";
+
+const Footer = () => {
+  return (
+    <footer className="container mx-auto text-primary text-xs flex flex-col gap-10 items-center pt-32 pb-20">
+      <Logo />
+      <NavLinks className="uppercase text-[#565656]" />
+      <p className="text-sm opacity-70 font-light">
+        &copy; All rights reserved by{" "}
+        <span className="text-accent font-medium">WordPressRiver</span>
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
