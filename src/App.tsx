4d0b97db4119a18ad82603ea15fd15892db1932e
LeulBerhanu <leulberhanua@gmail.com>
2024-11-19 00:44:37 +0300

    add Contact route

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import React from "react";
 import MainLayout from "./layouts/MainLayout";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Works from "./pages/Works";
+import Contact from "./pages/Contact";
 
 const App = () => {
   return (
@@ -12,6 +12,7 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/works" element={<Works />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
     </MainLayout>
   );
