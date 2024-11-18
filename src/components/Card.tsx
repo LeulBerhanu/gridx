e167ba1a0584cb09557114471c4e3f0934821d3a
LeulBerhanu <leulberhanua@gmail.com>
2024-11-18 17:38:17 +0300

    style card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,23 +1,31 @@
-import React, { ReactNode } from "react";
+import { ReactNode } from "react";
 import { Link } from "react-router-dom";
 
 interface Props {
   children: ReactNode;
   title?: string;
   description?: string;
+  absolute?: boolean;
   className?: string;
 }
 
-const Card = ({ children, title, description, className }: Props) => {
+const Card = ({ children, title, description, absolute, className }: Props) => {
   return (
-    <div className={`custom-bg p-6 text-white relative ${className}`}>
-      {children}
-      <div className="flex justify-between mt-4">
-        <div className="font-medium">
-          <h5 className="text-xs text-[#bcbcbc]">{description}</h5>
+    <div
+      className={`flex flex-col custom-bg py-6 px-6 text-white relative ${className} group cursor-pointer`}
+    >
+      <div className="flex-1">{children}</div>
+      <div className={`flex justify-between items-end ${!absolute && "mt-4"}`}>
+        <div className="font-medium space-y-2">
+          <h5 className="text-xs text-primary opacity-45">{description}</h5>
           <h2 className="text-xl">{title}</h2>
         </div>
-        <Link to="#">
+        <Link
+          to="#"
+          className={`${
+            absolute && "absolute right-6 bottom-8"
+          } opacity-50 group-hover:opacity-100 transition duration-100 ease-in-out`}
+        >
           <img
             src="https://wpriverthemes.com/gridx/wp-content/themes/gridx/assets/images/icon.svg"
             alt="link button"
