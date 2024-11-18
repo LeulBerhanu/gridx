7db2cc0aab840227a8121c4a341b47617d85eca2
LeulBerhanu <leulberhanua@gmail.com>
2024-11-19 00:43:54 +0300

    apply hover style on button

--- src/layouts/Navbar.tsx
+++ src/layouts/Navbar.tsx
@@ -6,7 +6,7 @@ const Navbar = () => {
     <nav className="container mx-auto flex justify-between py-3 items-center">
       <Logo />
       <NavLinks className="text-primary " />
-      <button className="bg-[#323232] text-white font-medium px-8 py-3 rounded-2xl hover:bg-white hover:text-black transition duration-200 ease-in-out ">
+      <button className="bg-[#323232] text-white font-medium px-8 py-3 rounded-2xl btn--hover ">
         Let's talk
       </button>
     </nav>
