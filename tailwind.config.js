56ab9440c3198e9775aa4d66415ac2b8b3433b21
LeulBerhanu <leulberhanua@gmail.com>
2024-11-18 13:09:09 +0300

    add custom styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+      },
+      backgroundImage: {
+        "primary-gradient": "linear-gradient(to right, #303030, #151515)",
+      },
+      borderRadius: {
+        "4xl": "30px",
+      },
+    },
   },
   plugins: [],
 };
