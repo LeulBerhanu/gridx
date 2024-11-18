c49c75e2a7dd9919c20ae777a814c276288e7e3b
LeulBerhanu <leulberhanua@gmail.com>
2024-11-18 17:39:12 +0300

    add Footer to MainLayout

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,11 +1,15 @@
 import React, { ReactNode } from "react";
 import Navbar from "./Navbar";
+import Footer from "./Footer";
 
 const MainLayout: React.FC<{ children: ReactNode }> = ({ children }) => {
   return (
     <>
-      <Navbar />
-      <main>{children}</main>
+      <header className="container mx-auto max-w-[1170px] px-6">
+        <Navbar />
+      </header>
+      <main className="container mx-auto max-w-[1170px] px-6">{children}</main>
+      <Footer />
     </>
   );
 };
