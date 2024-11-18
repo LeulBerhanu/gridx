590163e35aa99dce1b371469480852f55cf97cd0
LeulBerhanu <leulberhanua@gmail.com>
2024-11-18 17:38:28 +0300

    create Logo component

---
+++ src/components/Logo.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Logo = () => {
+  return (
+    <i>
+      <img
+        src="https://wpriverthemes.com/gridx/wp-content/themes/gridx/assets/images/logo.svg"
+        alt="logo"
+      />
+    </i>
+  );
+};
+
+export default Logo;
