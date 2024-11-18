3390dde3182855b6d28e06bb2809ee66b7f4b172
LeulBerhanu <leulberhanua@gmail.com>
2024-11-18 18:25:58 +0300

    add about route

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import MainLayout from "./layouts/MainLayout";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import About from "./pages/About";
 
 const App = () => {
   return (
     <MainLayout>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
       </Routes>
     </MainLayout>
   );
