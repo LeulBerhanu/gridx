6e0485c3bbc25c7f989e76b02c98134c18008562
LeulBerhanu <leulberhanua@gmail.com>
2024-11-18 18:47:43 +0300

    add Works route

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import MainLayout from "./layouts/MainLayout";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
+import Works from "./pages/Works";
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/works" element={<Works />} />
       </Routes>
     </MainLayout>
   );
