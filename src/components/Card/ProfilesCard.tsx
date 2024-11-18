b8da2e28e9d9fd7f883b68768314be6a16d2ad91
LeulBerhanu <leulberhanua@gmail.com>
2024-11-19 00:43:20 +0300

    fix gradient transition and add Link tag

--- src/components/Card/ProfilesCard.tsx
+++ src/components/Card/ProfilesCard.tsx
@@ -1,18 +1,24 @@
-import React from "react";
 import { CiBasketball } from "react-icons/ci";
 import { FiTwitter } from "react-icons/fi";
 import Card from "../Card";
+import { Link } from "react-router-dom";
 
 const ProfilesCard = () => {
   return (
     <Card title="Profiles" description="STAY WITH ME">
       <div className="flex gap-5 justify-around custom-bg px-4 py-6 shadow-lg">
-        <i className="icon--bg">
-          <CiBasketball className="icon" />
-        </i>
-        <i className="icon--bg">
-          <FiTwitter className="icon" />
-        </i>
+        <Link
+          to="#"
+          className="gradient-transition hover:text-black transition-all duration-200"
+        >
+          <CiBasketball className="size-20 p-6" />
+        </Link>
+        <Link
+          to="#"
+          className="gradient-transition hover:text-black transition-all duration-200"
+        >
+          <FiTwitter className="size-20 p-6" />
+        </Link>
       </div>
     </Card>
   );
