936b889e1bcfaecb21b223bf9a9f8ddc2c56200c
LeulBerhanu <leulberhanua@gmail.com>
2024-11-19 13:04:31 +0300

    fix style

--- src/components/NavLinks.tsx
+++ src/components/NavLinks.tsx
@@ -2,14 +2,17 @@ import { Link } from "react-router-dom";
 
 interface Props {
   className?: string;
+  footer?: boolean;
 }
 
 const transition = "hover:text-white transition duration-200 ease-in-out";
 
-const NavLinks = ({ className }: Props) => {
+const NavLinks = ({ className, footer }: Props) => {
   return (
     <div
-      className={`flex flex-col text-center  sm:flex-row sm:text-left gap-6 sm:gap-11 ${className}`}
+      className={`${
+        footer ? "flex" : "flex flex-col sm:flex-row"
+      } text-center sm:text-left gap-6 sm:gap-11 ${className}`}
     >
       <Link className={transition} to="/">
         Home
