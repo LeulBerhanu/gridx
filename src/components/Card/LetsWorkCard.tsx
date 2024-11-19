b1a07bac69525398d331616f375c964e4ad64a88
LeulBerhanu <leulberhanua@gmail.com>
2024-11-19 13:04:53 +0300

    fix style

--- src/components/Card/LetsWorkCard.tsx
+++ src/components/Card/LetsWorkCard.tsx
@@ -2,7 +2,7 @@ import Card from "../Card";
 
 const LetsWorkCard = () => {
   return (
-    <Card absolute className="pt-0 flex-1">
+    <Card absolute className="pt-0 flex-1 h-full">
       <div className="flex h-full flex-col justify-between">
         <div>
           <img
@@ -11,7 +11,7 @@ const LetsWorkCard = () => {
             alt="Star"
           />
         </div>
-        <h2 className="text-3xl sm:text-[42px] font-medium leading-tight mt-2 ">
+        <h2 className="text-3xl lg:text-[42px] font-medium leading-tight mt-2 ">
           Let's <br /> work <span className="text-accent">together.</span>
         </h2>
       </div>
