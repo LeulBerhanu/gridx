008fc05525037323a445cae8b64bd4c8118fa5c2
LeulBerhanu <leulberhanua@gmail.com>
2024-11-19 13:05:01 +0300

    fix style

--- src/layouts/Footer.tsx
+++ src/layouts/Footer.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import NavLinks from "../components/NavLinks";
 import Logo from "../components/Logo";
 
@@ -6,7 +5,7 @@ const Footer = () => {
   return (
     <footer className="container mx-auto text-primary text-xs flex flex-col gap-10 items-center pt-32 pb-20">
       <Logo />
-      <NavLinks className="uppercase text-[#565656]" />
+      <NavLinks className="uppercase text-[#565656]" footer />
       <p className="text-sm opacity-70 font-light">
         &copy; All rights reserved by{" "}
         <span className="text-accent font-medium">WordPressRiver</span>
