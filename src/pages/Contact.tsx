a7deee69d9e0253766e134581d766f58818eed9c
LeulBerhanu <leulberhanua@gmail.com>
2024-11-19 13:05:26 +0300

    add responsiveness

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -10,64 +10,73 @@ import { Link } from "react-router-dom";
 const Contact = () => {
   return (
     <section>
-      <div className="text-white pt-20 flex gap-20">
-        <div>
-          <h2 className="uppercase font-medium mb-7">Contact info</h2>
-          <div className="flex flex-col gap-20">
-            <div className="flex gap-6">
-              <div className="bg-primary-gradient px-6 py-5 rounded-xl ">
-                <MdOutlineEmail className="icon" />
+      <div className="text-white pt-20 flex flex-col lg:flex-row gap-20">
+        <div className="md:pl-6">
+          {/* contact info */}
+          <div>
+            <h2 className="uppercase font-medium mb-7">Contact info</h2>
+            <div className="flex flex-col sm:gap-20 gap-10">
+              <div className="flex gap-3 sm:gap-6">
+                <div className="bg-primary-gradient px-6 py-5 rounded-xl scale-75 sm:scale-100">
+                  <MdOutlineEmail className="icon" />
+                </div>
+                <div>
+                  <p className="uppercase text-sm text-primary mb-1">Mail us</p>
+                  <p className="font-medium opacity-80">info@lacazette.com</p>
+                  <p className="font-medium opacity-80">info@lacazetteA.com</p>
+                </div>
               </div>
-              <div>
-                <p className="uppercase text-sm text-primary mb-1">Mail us</p>
-                <p className="font-medium opacity-80">info@lacazette.com</p>
-                <p className="font-medium opacity-80">info@lacazetteA.com</p>
+              <div className="flex gap-3 sm:gap-6">
+                <div className="bg-primary-gradient px-6 py-5 rounded-xl scale-75 sm:scale-100">
+                  <FiPhone className="icon" />
+                </div>
+                <div>
+                  <p className="uppercase text-sm text-primary mb-1">
+                    contact us
+                  </p>
+                  <p className="font-medium opacity-80">+1 504-123-4567</p>
+                  <p className="font-medium opacity-80">+1 504-123-9876</p>
+                </div>
               </div>
-            </div>
-            <div className="flex gap-6">
-              <div className="bg-primary-gradient px-6 py-5 rounded-xl ">
-                <FiPhone className="icon" />
-              </div>
-              <div>
-                <p className="uppercase text-sm text-primary mb-1">
-                  contact us
-                </p>
-                <p className="font-medium opacity-80">+1 504-123-4567</p>
-                <p className="font-medium opacity-80">+1 504-123-9876</p>
-              </div>
-            </div>
-            <div className="flex gap-6 items-center">
-              <div className="bg-primary-gradient px-6 py-5 rounded-xl ">
-                <GrLocation className="icon" />
-              </div>
-              <div>
-                <p className="uppercase text-sm text-primary mb-1">location</p>
-                <p className="font-medium opacity-80">22 Baker Street, Texas</p>
-                <p className="font-medium opacity-80">United States</p>
-                <p className="font-medium opacity-80">W1U 3BW</p>
+              <div className="flex gap-3 sm:gap-6 items-center">
+                <div className="bg-primary-gradient px-6 py-5 rounded-xl scale-75 sm:scale-100">
+                  <GrLocation className="icon" />
+                </div>
+                <div>
+                  <p className="uppercase text-sm text-primary mb-1">
+                    location
+                  </p>
+                  <p className="font-medium opacity-80">
+                    22 Baker Street, Texas
+                  </p>
+                  <p className="font-medium opacity-80">United States</p>
+                  <p className="font-medium opacity-80">W1U 3BW</p>
+                </div>
               </div>
             </div>
           </div>
+
+          {/* social info */}
           <div className="mt-10 text-white">
             <h2 className="uppercase font-medium">Social info</h2>
-            <div className="flex gap-5 justify-around mt-5">
+            <div className="flex gap-5 lg:justify-around mt-5">
               <Link
                 to="#"
                 className="gradient-transition hover:text-black transition-all duration-200"
               >
-                <CiBasketball className="size-20 p-6" />
+                <CiBasketball className="size-14 p-4 sm:size-20 sm:p-6" />
               </Link>
               <Link
                 to="#"
                 className="gradient-transition hover:text-black transition-all duration-200"
               >
-                <FiTwitter className="size-20 p-6" />
+                <FiTwitter className="size-14 p-4 sm:size-20 sm:p-6" />
               </Link>
               <Link
                 to="#"
                 className="gradient-transition hover:text-black transition-all duration-200"
               >
-                <BsInstagram className="size-20 p-6" />
+                <BsInstagram className="size-14 p-4 sm:size-20 sm:p-6" />
               </Link>
             </div>
           </div>
--- src/pages/Works.tsx
+++ src/pages/Works.tsx
@@ -1,14 +1,34 @@
-import React from "react";
 import Card from "../components/Card";
+
+const imgStyle = "rounded-4xl w-full h-[290px] object-cover sm:h-auto";
 
 const Works = () => {
   return (
     <section>
-      <div className="flex gap-5 pt-20 justify-center">
+      <div className="flex flex-col sm:flex-row gap-5 pt-20 justify-center">
+        {/* section title for mobile */}
+        <div className="flex sm:hidden gap-1 items-center justify-center mb-4 ">
+          <img
+            decoding="async"
+            src="https://wpriverthemes.com/gridx/wp-content/themes/gridx/assets/images/star-2.png"
+            alt="Star"
+            className="object-contain size-7 lg:size-auto"
+          />
+          <p className="text-white uppercase font-semibold text-2xl sm:text-3xl lg:text-5xl xl:text-7xl">
+            all projects
+          </p>
+          <img
+            decoding="async"
+            src="https://wpriverthemes.com/gridx/wp-content/themes/gridx/assets/images/star-2.png"
+            alt="Star"
+            className="object-contain size-7 lg:size-auto"
+          />
+        </div>
+
         <div className="flex flex-col gap-5">
           <Card title="Dynamic" description="WEB DESIGNING" worksCard>
             <img
-              className="rounded-4xl"
+              className={imgStyle}
               decoding="async"
               src="https://picsum.photos/300/300"
               alt=""
@@ -18,10 +38,10 @@ const Works = () => {
             title="Diesel H1"
             description="PHOTOGRAPHY"
             worksCard
-            className="flex-1"
+            className="sm:flex-1"
           >
             <img
-              className="rounded-4xl h-full object-cover"
+              className={`rounded-4xl w-full h-[290px]  object-cover sm:h-full "`}
               src="https://picsum.photos/300/300"
               alt=""
             />
@@ -29,49 +49,50 @@ const Works = () => {
         </div>
 
         <div>
-          <div className="flex gap-1 items-center justify-center mb-4">
+          {/* section title */}
+          <div className="hidden sm:flex gap-1 items-center justify-center mb-4 ">
             <img
               decoding="async"
               src="https://wpriverthemes.com/gridx/wp-content/themes/gridx/assets/images/star-2.png"
               alt="Star"
-              className="object-contain"
+              className="object-contain size-7 lg:size-auto"
             />
-            <p className="text-white uppercase font-semibold text-7xl">
+            <p className="text-white uppercase font-semibold text-2xl sm:text-3xl lg:text-5xl xl:text-7xl">
               all projects
             </p>
             <img
               decoding="async"
               src="https://wpriverthemes.com/gridx/wp-content/themes/gridx/assets/images/star-2.png"
               alt="Star"
-              className="object-contain"
+              className="object-contain size-7 lg:size-auto"
             />
           </div>
 
-          <div className="grid grid-cols-2 gap-5">
+          <div className="flex flex-col sm:grid grid-cols-2 gap-5">
             <Card title="Seven Studio" description="MOBILE DESIGNING" worksCard>
               <img
-                className="rounded-4xl"
+                className={imgStyle}
                 src="https://picsum.photos/300/300"
                 alt=""
               />
             </Card>
             <Card title="Raven Studio" description="BRANDING" worksCard>
               <img
-                className="rounded-4xl"
+                className={imgStyle}
                 src="https://picsum.photos/300/300"
                 alt=""
               />
             </Card>
             <Card title="Submarine" description="MOBILE DESIGNING" worksCard>
               <img
-                className="rounded-4xl"
+                className={imgStyle}
                 src="https://picsum.photos/300/300"
                 alt=""
               />
             </Card>
             <Card title="Hydra Merc" description="WEB DESIGNING" worksCard>
               <img
-                className="rounded-4xl"
+                className={imgStyle}
                 src="https://picsum.photos/300/300"
                 alt=""
               />
