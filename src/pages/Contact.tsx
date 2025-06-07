06574539d330c743384437cd5137b96b1d44eb6f
LeulBerhanu <leulberhanua@gmail.com>
2025-06-08 02:11:56 +0300

    refactor Contact component to update contact details, enhance link handling, and improve social media icons

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,11 +1,10 @@
 import { FiPhone } from "react-icons/fi";
 import { GrLocation } from "react-icons/gr";
-import { MdOutlineEmail } from "react-icons/md";
 import ContactForm from "../components/ContactForm";
-import { CiBasketball } from "react-icons/ci";
-import { FiTwitter } from "react-icons/fi";
-import { BsInstagram } from "react-icons/bs";
+import { BsWhatsapp } from "react-icons/bs";
 import { Link } from "react-router-dom";
+import { CgMail } from "react-icons/cg";
+import { FaLinkedin } from "react-icons/fa";
 
 const Contact = () => {
   return (
@@ -18,12 +17,17 @@ const Contact = () => {
             <div className="flex flex-col sm:gap-20 gap-10">
               <div data-aos="zoom-in" className="flex gap-3 sm:gap-6">
                 <div className="bg-primary-gradient px-6 py-5 rounded-xl scale-75 sm:scale-100">
-                  <MdOutlineEmail className="icon" />
+                  <CgMail className="icon" />
                 </div>
                 <div>
                   <p className="uppercase text-sm text-primary mb-1">Mail us</p>
-                  <p className="font-medium opacity-80">info@lacazette.com</p>
-                  <p className="font-medium opacity-80">info@lacazetteA.com</p>
+                  <a
+                    href="mailto:leulberhanua@gmail.com"
+                    className="font-medium opacity-80 hover:underline"
+                  >
+                    leulberhanua@gmail.com
+                  </a>
+                  {/* <p className="font-medium opacity-80">info@lacazetteA.com</p> */}
                 </div>
               </div>
               <div
@@ -38,8 +42,13 @@ const Contact = () => {
                   <p className="uppercase text-sm text-primary mb-1">
                     contact us
                   </p>
-                  <p className="font-medium opacity-80">+1 504-123-4567</p>
-                  <p className="font-medium opacity-80">+1 504-123-9876</p>
+                  <a
+                    href="tel:+251938067108"
+                    className="font-medium opacity-80 hover:underline"
+                  >
+                    +251 938 067 108
+                  </a>
+                  {/* <p className="font-medium opacity-80">+1 504-123-9876</p> */}
                 </div>
               </div>
               <div
@@ -54,11 +63,11 @@ const Contact = () => {
                   <p className="uppercase text-sm text-primary mb-1">
                     location
                   </p>
-                  <p className="font-medium opacity-80">
-                    22 Baker Street, Texas
-                  </p>
-                  <p className="font-medium opacity-80">United States</p>
-                  <p className="font-medium opacity-80">W1U 3BW</p>
+                  {/* <p className="font-medium opacity-80">
+                    
+                  </p> */}
+                  <p className="font-medium opacity-80">Addis Ababa</p>
+                  <p className="font-medium opacity-80">Ethiopia</p>
                 </div>
               </div>
             </div>
@@ -67,29 +76,29 @@ const Contact = () => {
           {/* social info */}
           <div className="mt-10 text-white">
             <h2 className="uppercase font-medium">Social info</h2>
-            <div className="flex gap-5 lg:justify-around mt-5">
-              <Link
+            <div className="flex gap-5  mt-5">
+              {/* <Link
                 to="#"
                 className="gradient-transition hover:text-black transition-all duration-200"
                 data-aos="zoom-in"
               >
                 <CiBasketball className="size-14 p-4 sm:size-20 sm:p-6" />
-              </Link>
+              </Link> */}
               <Link
                 to="#"
-                className="gradient-transition hover:text-black transition-all duration-200"
+                className="gradient-transition hover:text-blue-600 transition-all duration-200"
                 data-aos="zoom-in"
                 data-aos-delay="100"
               >
-                <FiTwitter className="size-14 p-4 sm:size-20 sm:p-6" />
+                <FaLinkedin className="size-14 p-4 sm:size-20 sm:p-6" />
               </Link>
               <Link
-                to="#"
-                className="gradient-transition hover:text-black transition-all duration-200"
+                to="https://wa.me/251938067108"
+                className="gradient-transition hover:text-green-800 transition-all duration-200"
                 data-aos="zoom-in"
                 data-aos-delay="200"
               >
-                <BsInstagram className="size-14 p-4 sm:size-20 sm:p-6" />
+                <BsWhatsapp className="size-14 p-4 sm:size-20 sm:p-6" />
               </Link>
             </div>
           </div>
