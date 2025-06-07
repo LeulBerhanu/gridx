c23fa06bf41a3d7dcad4655a59aa634229e42add
LeulBerhanu <leulberhanua@gmail.com>
2025-06-08 02:09:57 +0300

    refactor Card component to improve link handling and structure

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -8,6 +8,9 @@ interface Props {
   absolute?: boolean;
   worksCard?: boolean;
   className?: string;
+  link?: string;
+  nolink?: boolean;
+  noNewTab?: boolean;
 }
 
 const Card = ({
@@ -16,6 +19,9 @@ const Card = ({
   description,
   absolute,
   worksCard,
+  link,
+  nolink,
+  noNewTab,
   className,
   ...props
 }: Props) => {
@@ -24,34 +30,45 @@ const Card = ({
       {...props}
       className={`${
         worksCard ? "p-4" : "py-6 px-6"
-      } flex flex-col custom-bg  text-white relative ${className} group cursor-pointer`}
+      } flex flex-col custom-bg  text-white relative ${className} group  `}
     >
-      <div className="flex-1">{children}</div>
-      <div className={`flex justify-between items-end ${!absolute && "mt-4"}`}>
+      <Link
+        to={link || "#"}
+        target={nolink || noNewTab ? "_self" : "_blank"}
+        className={`group relative ${nolink ? "cursor-default" : ""}`}
+      >
+        <div className="flex-1 overflow-hidden">{children}</div>
         <div
-          className={`${worksCard ? "space-y-0" : "space-y-2"} font-medium `}
+          className={`flex justify-between items-end ${!absolute && "mt-4"}`}
         >
-          <h5
-            className={`${
-              worksCard ? "text-sm" : "text-xs"
-            }  text-primary opacity-45`}
+          <div
+            className={`${worksCard ? "space-y-0" : "space-y-2"} font-medium `}
           >
-            {description}
-          </h5>
-          <h2 className="text-xl">{title}</h2>
+            <h5
+              className={`${
+                worksCard ? "text-sm" : "text-xs"
+              }  text-primary opacity-45`}
+            >
+              {description}
+            </h5>
+            <h2 className="text-xl">{title}</h2>
+          </div>
+          {nolink ? null : (
+            <Link
+              to={link || "#"}
+              target="_blank"
+              className={`${
+                absolute && "absolute right-6 bottom-8"
+              } opacity-50 group-hover:opacity-100 transition duration-100 ease-in-out`}
+            >
+              <img
+                src="https://wpriverthemes.com/gridx/wp-content/themes/gridx/assets/images/icon.svg"
+                alt="link button"
+              />
+            </Link>
+          )}
         </div>
-        <Link
-          to="#"
-          className={`${
-            absolute && "absolute right-6 bottom-8"
-          } opacity-50 group-hover:opacity-100 transition duration-100 ease-in-out`}
-        >
-          <img
-            src="https://wpriverthemes.com/gridx/wp-content/themes/gridx/assets/images/icon.svg"
-            alt="link button"
-          />
-        </Link>
-      </div>
+      </Link>
     </div>
   );
 };
